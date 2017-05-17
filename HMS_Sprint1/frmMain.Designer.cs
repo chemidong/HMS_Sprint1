b4761f2bb291684c4d2d4e34a7497b7f4d0e74af
chemidong <chemidong@gmail.com>
2017-05-17 20:07:24 +0900

    중간 업로드

--- HMS_Sprint1/frmMain.Designer.cs
+++ HMS_Sprint1/frmMain.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.butSignin = new System.Windows.Forms.Button();
             this.butSignup = new System.Windows.Forms.Button();
@@ -36,16 +35,6 @@
             this.picLogin = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.picLogin)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Adobe Heiti Std R", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(55, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(482, 90);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nayeon Hotel";
             // 
             // txtPassword
             // 
@@ -81,6 +70,7 @@
             this.txtID.Name = "txtID";
             this.txtID.Size = new System.Drawing.Size(400, 49);
             this.txtID.TabIndex = 7;
+            this.txtID.TextChanged += new System.EventHandler(this.txtID_TextChanged);
             // 
             // picLogin
             // 
@@ -101,7 +91,6 @@
             this.Controls.Add(this.butSignup);
             this.Controls.Add(this.butSignin);
             this.Controls.Add(this.txtPassword);
-            this.Controls.Add(this.label1);
             this.Name = "frmMain";
             this.Text = "Twice Nayeon HMS";
             this.Load += new System.EventHandler(this.frmMain_Load);
@@ -112,8 +101,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Button butSignin;
         private System.Windows.Forms.Button butSignup;
--- HMS_Sprint1/frmMain.cs
+++ HMS_Sprint1/frmMain.cs
@@ -20,12 +20,57 @@ namespace HMS_Sprint1
         private void frmMain_Load(object sender, EventArgs e)
         {
             picLogin.Image =  Image.FromFile(System.IO.Directory.GetCurrentDirectory() + @"\img\pic1.jpg");
-        }
+            this.txtID.Enter += new EventHandler(txtID_Enter);
+            this.txtID.Leave += new EventHandler(txtID_Leave);
+            txtID_SetText();
 
+            this.txtPassword.Enter += new EventHandler(txtPassword_Enter);
+            this.txtPassword.Leave += new EventHandler(txtPassword_Leave);
+            txtPassword_SetText();
+
+            butSignin.Focus();
+        }
+        private void txtID_Enter(object sender, EventArgs e)
+        {
+            txtID.Text = "";
+            txtID.ForeColor = Color.Black;
+        }
+        private void txtID_Leave(object sender, EventArgs e)
+        {
+            if (txtID.Text.Trim() == "")
+                txtID_SetText();
+        }
+        private void txtID_SetText()
+        {
+            if (txtID.ForeColor == Color.Black)
+                return;
+            this.txtID.Text = "ID";
+            txtID.ForeColor = Color.Gray;
+        }
+        private void txtPassword_Enter(object sender, EventArgs e)
+        {
+            txtPassword.Text = "";
+            txtPassword.ForeColor = Color.Black;
+        }
+        private void txtPassword_Leave(object sender, EventArgs e)
+        {
+            if (txtPassword.Text.Trim() == "")
+                txtPassword_SetText();
+        }
+        private void txtPassword_SetText()
+        {
+            this.txtPassword.Text = "Password";
+            txtPassword.ForeColor = Color.Gray;
+        }
         private void butSignup_Click(object sender, EventArgs e)
         {
             frmSignup frmSign = new frmSignup();
             frmSign.Show();
+        }
+
+        private void txtID_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
