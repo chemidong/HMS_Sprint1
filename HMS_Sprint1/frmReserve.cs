08351c4cace51165e8cff186b10febeed0585d9f
jongminpark <qwaqwa93@kaist.ac.kr>
2017-05-17 20:15:00 +0900

    frmReserve
    empty form

---
+++ HMS_Sprint1/frmReserve.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HMS_Sprint1
+{
+    public partial class frmReserve : Form
+    {
+        public frmReserve()
+        {
+            InitializeComponent();
+        }
+
+        private void frmReserve_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
